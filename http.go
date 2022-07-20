b1ce24ecd8b78a0275f3eeda9101c5e638fc8c73
smallmenu <smallmenu@gmail.com>
2022-07-20 14:55:28 +0800

    * update Http

--- http.go
+++ http.go
@@ -3,7 +3,9 @@ package fun
 import (
 	"crypto/tls"
 	"errors"
+	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
@@ -11,69 +13,141 @@ import (
 )
 
 const (
-	HttpDefaultTimeOut   = 5000
+	HttpDefaultTimeOut   = 10000
 	HttpDefaultUserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 )
 
+// HttpDefaultTransport 定义了一个默认全局使用的 http.Transport
+var HttpDefaultTransport = &http.Transport{
+	DialContext:           (&net.Dialer{Timeout: time.Second}).DialContext,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          200,
+	MaxIdleConnsPerHost:   5,
+	IdleConnTimeout:       60 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+	TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
+}
+
 type HttpReq struct {
-	UserAgent   string
-	Headers     map[string]string
-	ProxyString string
+	UserAgent string
+	Headers   map[string]string
+	Transport http.RoundTripper
 }
 
 type HttpResp struct {
 	Success       bool
 	StatusCode    int
-	Body          string
+	Body          []byte
 	ContentLength int64
 	Headers       *http.Header
 }
 
 // HttpGet 参数为请求地址（HttpReq, 超时时间）
 // HttpGet(url)、HttpGet(url, timeout)、HttpGet(url, httpReq, timeout)
-// 返回值为请求内容 String，错误信息
-func HttpGet(urlStr string, args ...any) (string, error) {
+// 返回 body，错误信息
+func HttpGet(urlStr string, args ...any) ([]byte, error) {
 	l := len(args)
 
 	switch l {
 	case 0:
-		return HttpGetBody(urlStr, nil, 0)
+		return HttpGetDo(urlStr, nil, 0)
 	case 1:
 		timeout := ToInt(args[0])
-		return HttpGetBody(urlStr, nil, timeout)
+		return HttpGetDo(urlStr, nil, timeout)
 	case 2:
 		timeout := ToInt(args[1])
 		switch v := args[0].(type) {
 		case *HttpReq:
-			return HttpGetBody(urlStr, v, timeout)
+			return HttpGetDo(urlStr, v, timeout)
 		}
 	}
 
-	return "", errors.New("HttpGet() 参数错误")
+	return nil, errors.New("HttpGet() params error")
 }
 
-// HttpPost 参数为请求地址（Form 数据 map[string]string，HttpReq，超时时间）
-// HttpPost(url)、HttpPost(url, timeout)、HttpPost(url, posts)、HttpPost(url, posts, timeout)、HttpPost(url, posts, httpReq, timeout)
-// 返回值为请求内容 String，错误信息
-func HttpPost(urlStr string, args ...any) (string, error) {
+// HttpDelete 参数为请求地址（HttpReq, 超时时间）
+// HttpDelete(url)、HttpDelete(url, timeout)、HttpDelete(url, httpReq, timeout)
+// 返回 body，错误信息
+func HttpDelete(urlStr string, args ...any) ([]byte, error) {
 	l := len(args)
 
 	switch l {
 	case 0:
-		return HttpPostBody(urlStr, nil, nil, 0)
+		return HttpDeleteDo(urlStr, nil, 0)
+	case 1:
+		timeout := ToInt(args[0])
+		return HttpDeleteDo(urlStr, nil, timeout)
+	case 2:
+		timeout := ToInt(args[1])
+		switch v := args[0].(type) {
+		case *HttpReq:
+			return HttpDeleteDo(urlStr, v, timeout)
+		}
+	}
+
+	return nil, errors.New("HttpDelete params error")
+}
+
+// HttpPost 参数为请求地址（body io.Reader，HttpReq，超时时间）
+// HttpPost(url)、HttpPost(url, timeout)、HttpPost(url, body)、HttpPost(url, body, timeout)、HttpPostForm(url, body, httpReq, timeout)
+// 返回 body，错误信息
+func HttpPost(urlStr string, args ...any) ([]byte, error) {
+	l := len(args)
+
+	switch l {
+	case 0:
+		return HttpPostDo(urlStr, nil, nil, 0)
 	case 1:
 		switch v := args[0].(type) {
 		case int:
 			timeout := ToInt(args[0])
-			return HttpPostBody(urlStr, nil, nil, timeout)
+			return HttpPostDo(urlStr, nil, nil, timeout)
+		case io.Reader:
+			return HttpPostDo(urlStr, v, nil, 0)
+		}
+	case 2:
+		switch v := args[0].(type) {
+		case io.Reader:
+			timeout := ToInt(args[1])
+			return HttpPostDo(urlStr, v, nil, timeout)
+		}
+	case 3:
+		switch v := args[0].(type) {
+		case io.Reader:
+			switch h := args[1].(type) {
+			case *HttpReq:
+				timeout := ToInt(args[2])
+				return HttpPostDo(urlStr, v, h, timeout)
+			}
+		}
+	}
+
+	return nil, errors.New("HttpPost params error")
+}
+
+// HttpPostForm 参数为请求地址（Form 数据 map[string]string，HttpReq，超时时间）
+// HttpPostForm(url)、HttpPostForm(url, timeout)、HttpPostForm(url, posts)、HttpPostForm(url, posts, timeout)、HttpPostForm(url, posts, httpReq, timeout)
+// 返回 body，错误信息
+func HttpPostForm(urlStr string, args ...any) ([]byte, error) {
+	l := len(args)
+
+	switch l {
+	case 0:
+		return HttpPostFormDo(urlStr, nil, nil, 0)
+	case 1:
+		switch v := args[0].(type) {
+		case int:
+			timeout := ToInt(args[0])
+			return HttpPostFormDo(urlStr, nil, nil, timeout)
 		case map[string]string:
-			return HttpPostBody(urlStr, v, nil, 0)
+			return HttpPostFormDo(urlStr, v, nil, 0)
 		}
 	case 2:
 		switch v := args[0].(type) {
 		case map[string]string:
 			timeout := ToInt(args[1])
-			return HttpPostBody(urlStr, v, nil, timeout)
+			return HttpPostFormDo(urlStr, v, nil, timeout)
 		}
 	case 3:
 		switch v := args[0].(type) {
@@ -81,35 +155,35 @@ func HttpPost(urlStr string, args ...any) (string, error) {
 			switch h := args[1].(type) {
 			case *HttpReq:
 				timeout := ToInt(args[2])
-				return HttpPostBody(urlStr, v, h, timeout)
+				return HttpPostFormDo(urlStr, v, h, timeout)
 			}
 		}
 	}
 
-	return "", errors.New("HttpPost() 参数错误")
+	return nil, errors.New("HttpPostFrom params error")
 }
 
 // HttpPostJson 参数为请求地址（Json 数据 string，HttpReq, 超时时间）
 // HttpPostJson(url)、HttpPostJson(url, timeout)、HttpPostJson(url, json)、HttpPost(url, json, timeout)、HttpPost(url, json, httpReq, timeout)
-// 返回值为请求内容 String，错误信息
-func HttpPostJson(urlStr string, args ...any) (string, error) {
+// 返回 body，错误信息
+func HttpPostJson(urlStr string, args ...any) ([]byte, error) {
 	l := len(args)
 	switch l {
 	case 0:
-		return HttpPostJsonBody(urlStr, "{}", nil, 0)
+		return HttpPostJsonDo(urlStr, "{}", nil, 0)
 	case 1:
 		switch v := args[0].(type) {
 		case int:
 			timeout := ToInt(args[0])
-			return HttpPostJsonBody(urlStr, "{}", nil, timeout)
+			return HttpPostJsonDo(urlStr, "{}", nil, timeout)
 		case string:
-			return HttpPostJsonBody(urlStr, v, nil, 0)
+			return HttpPostJsonDo(urlStr, v, nil, 0)
 		}
 	case 2:
 		switch v := args[0].(type) {
 		case string:
 			timeout := ToInt(args[1])
-			return HttpPostJsonBody(urlStr, v, nil, timeout)
+			return HttpPostJsonDo(urlStr, v, nil, timeout)
 		}
 	case 3:
 		switch v := args[0].(type) {
@@ -117,42 +191,64 @@ func HttpPostJson(urlStr string, args ...any) (string, error) {
 			switch h := args[1].(type) {
 			case *HttpReq:
 				timeout := ToInt(args[2])
-				return HttpPostJsonBody(urlStr, v, h, timeout)
+				return HttpPostJsonDo(urlStr, v, h, timeout)
 			}
 		}
 	}
 
-	return "", errors.New("HttpPostJson() 参数错误")
+	return nil, errors.New("HttpPostJson params error")
 }
 
-// HttpGetBody Http Get 请求，参数为请求地址，HttpReq，超时时间(毫秒)
-// 返回请求内容 String，错误信息
-func HttpGetBody(urlStr string, r *HttpReq, timeout int) (string, error) {
+// HttpGetDo Http Get 请求，参数为请求地址，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpGetDo(urlStr string, r *HttpReq, timeout int) ([]byte, error) {
 	resp, err := HttpGetResp(urlStr, r, timeout)
 	if err != nil {
-		return "", err
+		return nil, err
 	} else {
 		return resp.Body, nil
 	}
 }
 
-// HttpPostBody Http Post Form，参数为请求地址，Form 数据 map[string]string，HttpReq，超时时间(毫秒)
-// 返回请求内容 String，错误信息
-func HttpPostBody(urlStr string, posts map[string]string, r *HttpReq, timeout int) (string, error) {
-	resp, err := HttpPostResp(urlStr, posts, r, timeout)
+// HttpDeleteDo Http Delete 请求，参数为请求地址，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpDeleteDo(urlStr string, r *HttpReq, timeout int) ([]byte, error) {
+	resp, err := HttpDeleteResp(urlStr, r, timeout)
 	if err != nil {
-		return "", err
+		return nil, err
 	} else {
 		return resp.Body, nil
 	}
 }
 
-// HttpPostJsonBody Http Post Json 请求，参数为请求地址，Json 数据 string，HttpReq，超时时间(毫秒)
-// 返回请求内容 String，错误信息
-func HttpPostJsonBody(urlStr string, json string, r *HttpReq, timeout int) (string, error) {
+// HttpPostDo Http Post，参数为请求地址，body io.Reader，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpPostDo(urlStr string, body io.Reader, r *HttpReq, timeout int) ([]byte, error) {
+	resp, err := HttpPostResp(urlStr, body, r, timeout)
+	if err != nil {
+		return nil, err
+	} else {
+		return resp.Body, nil
+	}
+}
+
+// HttpPostFormDo Http Post Form，参数为请求地址，Form 数据 map[string]string，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpPostFormDo(urlStr string, posts map[string]string, r *HttpReq, timeout int) ([]byte, error) {
+	resp, err := HttpPostFormResp(urlStr, posts, r, timeout)
+	if err != nil {
+		return nil, err
+	} else {
+		return resp.Body, nil
+	}
+}
+
+// HttpPostJsonDo Http Post Json 请求，参数为请求地址，Json 数据 string，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpPostJsonDo(urlStr string, json string, r *HttpReq, timeout int) ([]byte, error) {
 	resp, err := HttpPostJsonResp(urlStr, json, r, timeout)
 	if err != nil {
-		return "", err
+		return nil, err
 	} else {
 		return resp.Body, nil
 	}
@@ -161,100 +257,39 @@ func HttpPostJsonBody(urlStr string, json string, r *HttpReq, timeout int) (stri
 // HttpGetResp Http Get 请求，参数为请求地址，HttpReq，超时时间(毫秒)
 // 返回 HttpResp，错误信息
 func HttpGetResp(urlStr string, r *HttpReq, timeout int) (*HttpResp, error) {
-	httpResp := &HttpResp{
-		Success:       false,
-		StatusCode:    0,
-		Body:          "",
-		ContentLength: 0,
-		Headers:       nil,
-	}
-
-	if timeout == 0 {
-		timeout = HttpDefaultTimeOut
-	}
-
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	// 设置 Proxy 代理
-	if r != nil && r.ProxyString != "" {
-		proxy, _ := url.Parse(r.ProxyString)
-		transport.Proxy = http.ProxyURL(proxy)
-	}
-
-	client := &http.Client{
-		Timeout:   time.Duration(timeout) * time.Millisecond,
-		Transport: transport,
-	}
-
 	req, err := http.NewRequest(http.MethodGet, urlStr, nil)
 	if err != nil {
-		return httpResp, err
+		return nil, err
 	}
 
-	// 处理请求头
-	headers := make(map[string]string)
-	if r != nil && r.UserAgent != "" {
-		r.Headers["User-Agent"] = r.UserAgent
-	}
-	if r != nil && r.Headers != nil && len(r.Headers) > 0 {
-		headers = r.Headers
-		if _, exist := headers["User-Agent"]; !exist {
-			headers["User-Agent"] = HttpDefaultUserAgent
-		}
-	} else {
-		headers["User-Agent"] = HttpDefaultUserAgent
-	}
-	for k, v := range headers {
-		req.Header.Set(k, v)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return httpResp, err
-	}
-
-	httpResp.StatusCode = resp.StatusCode
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		httpResp.Success = true
-	}
-	httpResp.Headers = &resp.Header
-	httpResp.ContentLength = resp.ContentLength
-
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		return httpResp, err
-	} else {
-		httpResp.Body = BytesToString(body)
-	}
-
-	return httpResp, nil
+	return HttpDoResp(req, r, timeout)
 }
 
-// HttpPostResp Http Post Form，参数为请求地址，Form 数据 map[string]string，HttpReq，超时时间(毫秒)
+// HttpDeleteResp Http Delete 请求，参数为请求地址，HttpReq，超时时间(毫秒)
 // 返回 HttpResp，错误信息
-func HttpPostResp(urlStr string, posts map[string]string, r *HttpReq, timeout int) (*HttpResp, error) {
-	httpResp := &HttpResp{
-		Success:       false,
-		StatusCode:    0,
-		Body:          "",
-		ContentLength: 0,
-		Headers:       nil,
+func HttpDeleteResp(urlStr string, r *HttpReq, timeout int) (*HttpResp, error) {
+	req, err := http.NewRequest(http.MethodDelete, urlStr, nil)
+	if err != nil {
+		return nil, err
 	}
 
-	if timeout == 0 {
-		timeout = HttpDefaultTimeOut
+	return HttpDoResp(req, r, timeout)
+}
+
+// HttpPostResp Http Post，参数为请求地址，body io.Reader，HttpReq，超时时间(毫秒)
+// 返回 HttpResp，错误信息
+func HttpPostResp(urlStr string, body io.Reader, r *HttpReq, timeout int) (*HttpResp, error) {
+	req, err := http.NewRequest(http.MethodPost, urlStr, body)
+	if err != nil {
+		return nil, err
 	}
 
-	client := &http.Client{
-		Timeout: time.Duration(timeout) * time.Millisecond,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
+	return HttpDoResp(req, r, timeout)
+}
 
+// HttpPostFormResp Http Post Form，参数为请求地址，Form 数据 map[string]string，HttpReq，超时时间(毫秒)
+// 返回 HttpResp，错误信息
+func HttpPostFormResp(urlStr string, posts map[string]string, r *HttpReq, timeout int) (*HttpResp, error) {
 	data := url.Values{}
 	if posts != nil && len(posts) > 0 {
 		for k, v := range posts {
@@ -262,80 +297,59 @@ func HttpPostResp(urlStr string, posts map[string]string, r *HttpReq, timeout in
 		}
 	}
 
-	// 处理 Form
 	req, err := http.NewRequest(http.MethodPost, urlStr, strings.NewReader(data.Encode()))
 	if err != nil {
-		return httpResp, err
+		return nil, err
 	}
 
-	// 处理请求头
-	headers := make(map[string]string)
-	if r != nil && r.UserAgent != "" {
-		r.Headers["User-Agent"] = r.UserAgent
-	}
-	if r != nil && r.Headers != nil && len(r.Headers) > 0 {
-		headers = r.Headers
-		if _, exist := headers["User-Agent"]; !exist {
-			headers["User-Agent"] = HttpDefaultUserAgent
-		}
-	} else {
-		headers["User-Agent"] = HttpDefaultUserAgent
-	}
-	for k, v := range headers {
-		req.Header.Set(k, v)
-	}
+	req.Header.Set("Content-Type", MimePostForm)
 
-	req.Header.Set("Content-Type", MimeMultipartPostForm)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return httpResp, err
-	}
-
-	httpResp.StatusCode = resp.StatusCode
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		httpResp.Success = true
-	}
-	httpResp.Headers = &resp.Header
-	httpResp.ContentLength = resp.ContentLength
-
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		return httpResp, err
-	} else {
-		httpResp.Body = BytesToString(body)
-	}
-
-	return httpResp, nil
+	return HttpDoResp(req, r, timeout)
 }
 
 // HttpPostJsonResp Http Post Json 请求，参数为请求地址，Json 数据 string，HttpReq，超时时间(毫秒)
 // 返回 HttpResp，错误信息
 func HttpPostJsonResp(urlStr string, json string, r *HttpReq, timeout int) (*HttpResp, error) {
-	httpResp := &HttpResp{
-		Success:       false,
-		StatusCode:    0,
-		Body:          "",
-		ContentLength: 0,
-		Headers:       nil,
+	req, err := http.NewRequest(http.MethodPost, urlStr, strings.NewReader(json))
+	if err != nil {
+		return nil, err
 	}
 
+	req.Header.Set("Content-Type", MimeJson)
+
+	return HttpDoResp(req, r, timeout)
+}
+
+// HttpDo Http 请求，参数为 http.Request，HttpReq，超时时间(毫秒)
+// 返回 body，错误信息
+func HttpDo(req *http.Request, r *HttpReq, timeout int) ([]byte, error) {
+	resp, err := HttpDoResp(req, r, timeout)
+	if err != nil {
+		return nil, err
+	} else {
+		return resp.Body, nil
+	}
+}
+
+// HttpDoResp Http 请求，参数为 http.Request，HttpReq，超时时间(毫秒)
+// 返回 HttpResp，错误信息
+func HttpDoResp(req *http.Request, r *HttpReq, timeout int) (*HttpResp, error) {
 	if timeout == 0 {
 		timeout = HttpDefaultTimeOut
 	}
 
-	client := &http.Client{
-		Timeout: time.Duration(timeout) * time.Second,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-
-	// 处理 Json
-	req, err := http.NewRequest(http.MethodPost, urlStr, strings.NewReader(json))
-	if err != nil {
-		return httpResp, err
+	// NewClient
+	var client *http.Client
+	if r != nil && r.Transport != nil {
+		client = &http.Client{
+			Timeout:   time.Duration(timeout) * time.Millisecond,
+			Transport: r.Transport,
+		}
+	} else {
+		client = &http.Client{
+			Timeout:   time.Duration(timeout) * time.Millisecond,
+			Transport: HttpDefaultTransport,
+		}
 	}
 
 	// 处理请求头
@@ -355,7 +369,14 @@ func HttpPostJsonResp(urlStr string, json string, r *HttpReq, timeout int) (*Htt
 		req.Header.Set(k, v)
 	}
 
-	req.Header.Set("Content-Type", MimeJson)
+	// HttpResp
+	httpResp := &HttpResp{
+		Success:       false,
+		StatusCode:    0,
+		Body:          nil,
+		ContentLength: 0,
+		Headers:       nil,
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -374,7 +395,7 @@ func HttpPostJsonResp(urlStr string, json string, r *HttpReq, timeout int) (*Htt
 	if err != nil {
 		return httpResp, err
 	} else {
-		httpResp.Body = BytesToString(body)
+		httpResp.Body = body
 	}
 
 	return httpResp, nil
--- http_test.go
+++ http_test.go
@@ -1,6 +1,8 @@
 package fun
 
 import (
+	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -12,99 +14,117 @@ func TestHttpGet(t *testing.T) {
 	urlStr := TestUrl + "/get"
 
 	body, _ := HttpGet(urlStr)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
-	body, _ = HttpGet(urlStr + "?query1=abc1&query2=abc2")
-	t.Log(body)
+	body, _ = HttpGet(urlStr + "?query1=value1&query2=value2")
+	t.Log(BytesToString(body))
 
 	// 超时时间
 	body, _ = HttpGet(urlStr, 1000)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
 	// Headers 与超时时间
 	req := &HttpReq{
+		UserAgent: "test-ua",
 		Headers: map[string]string{
-			"User-Agent": "test-ua",
-			"X-Header":   "test-header",
+			"X-Header": "test-header",
 		},
 	}
-	body, _ = HttpGet(urlStr, req, 1000)
-	t.Log(body)
+	body, _ = HttpGet(urlStr+"?query1=value1", req, 1000)
+	t.Log(BytesToString(body))
 
 	// 错误的 Headers
 	body, err := HttpGet(urlStr, "error header", 1000)
-	t.Log(body)
+	t.Log(BytesToString(body))
 	t.Log(err)
 
 }
 
-func TestHttpGetProxy(t *testing.T) {
-	urlStr := "https://www.ip138.com/"
-	req := &HttpReq{
-		Headers: map[string]string{
-			"User-Agent": "test-ua",
-			"X-Header":   "test-header",
-		},
-		ProxyString: "http://username:password@localhost:8080",
-	}
-	body, _ := HttpGet(urlStr, req, 5000)
-	t.Log(body)
-}
-
-func TestHttpPost(t *testing.T) {
+func TestHttpPostForm(t *testing.T) {
 	urlStr := TestUrl + "/post"
-
-	body, _ := HttpPost(urlStr)
-	t.Log(body)
-
-	// 超时时间
-	body, _ = HttpPost(urlStr, 1000)
-	t.Log(body)
 
 	// Post 数据
 	posts := map[string]string{
 		"post1": "post1",
-		"post2": "post2",
 	}
-	body, _ = HttpPost(urlStr, posts)
-	t.Log(body)
+	body, _ := HttpPostForm(urlStr, posts)
+	t.Log(BytesToString(body))
 
 	// Post 数据与超时时间
-	body, _ = HttpPost(urlStr, posts, 1000)
-	t.Log(body)
+	body, _ = HttpPostForm(urlStr, posts, 1000)
+	t.Log(BytesToString(body))
 
 	req := &HttpReq{
+		UserAgent: "test-ua",
 		Headers: map[string]string{
-			"User-Agent": "test-ua",
-			"X-Header":   "test-header",
+			"X-Header": "test-header",
 		},
 	}
-	body, _ = HttpPost(urlStr, posts, req, 1000)
-	t.Log(body)
+	body, _ = HttpPostForm(urlStr, posts, req, 1000)
+	t.Log(BytesToString(body))
 }
 
 func TestHttpPostJson(t *testing.T) {
 	urlStr := TestUrl + "/postJson"
 
 	body, _ := HttpPostJson(urlStr)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
 	body, _ = HttpPostJson(urlStr, 1000)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
-	json := `{"param1":"json1","param2":"json2"}`
+	json := `{"username":"admin","email":"admin@test.com"}`
 	body, _ = HttpPostJson(urlStr, json)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
 	body, _ = HttpPostJson(urlStr, json, 1000)
-	t.Log(body)
+	t.Log(BytesToString(body))
 
 	req := &HttpReq{
+		UserAgent: "test-ua",
 		Headers: map[string]string{
-			"User-Agent": "test-ua",
-			"X-Header":   "test-header",
+			"X-Header": "test-header",
 		},
 	}
 	body, _ = HttpPostJson(urlStr, json, req, 1000)
-	t.Log(body)
+	t.Log(BytesToString(body))
+}
+
+func TestHttpPost(t *testing.T) {
+	urlStr := TestUrl + "/post"
+
+	data := url.Values{}
+	data.Set("post1", "post1")
+	data.Set("post2", "post2")
+	b := strings.NewReader(data.Encode())
+
+	req := &HttpReq{
+		UserAgent: "test-ua",
+		Headers: map[string]string{
+			"Content-Type": MimePostForm,
+		},
+	}
+
+	body, err := HttpPost(urlStr, b, req, 1000)
+	t.Log(BytesToString(body))
+	t.Log(err)
+}
+
+func TestHttpDelete(t *testing.T) {
+	urlStr := TestUrl + "/delete/path"
+
+	// Headers 与超时时间
+	req := &HttpReq{
+		UserAgent: "test-ua",
+		Headers: map[string]string{
+			"X-Header": "test-header",
+		},
+	}
+	body, _ := HttpDelete(urlStr+"?query1=value1", req, 1000)
+	t.Log(BytesToString(body))
+
+	// 错误的 Headers
+	body, err := HttpDelete(urlStr, "error header", 1000)
+	t.Log(BytesToString(body))
+	t.Log(err)
 }
