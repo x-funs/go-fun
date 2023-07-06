3d2923b3083e9e73a07164c04db9b0109e1d5b5c
smallmenu <smallmenu@163.com>
2023-07-06 09:54:33 +0800

    fix: add file.go

---
+++ file_test.go
@@ -0,0 +1,33 @@
+package fun
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMkdir(t *testing.T) {
+	tempDir := os.TempDir()
+	dir := filepath.Join(tempDir, "test")
+	assert.Equal(t, nil, Mkdir(dir, 0755))
+}
+
+func TestWriteFileAppend(t *testing.T) {
+	tempDir := os.TempDir()
+	dir := filepath.Join(tempDir, "test")
+
+	_ = Mkdir(dir, 0755)
+
+	filename := filepath.Join(dir, "test.txt")
+
+	data := []byte("All the data \nI wish to write to a file")
+
+	t.Log(filename)
+
+	for i := 0; i < 3; i++ {
+		_ = WriteFileAppend(filename, data, 0777, false)
+		_ = WriteFileAppend(filename, []byte("\n"), 0777, false)
+	}
+}
